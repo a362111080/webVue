e07b9a82c0ede8332b46c60f78f55882bdb22b64
a362111080 <362111080@qq.com>
2020-06-08 01:42:09 +0800

    完成用户列表的开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,147 @@
 <template>
-    <div>
-        <el-button type="info" @click="logout">退出</el-button>
-        
-    </div>
+  
+    <el-container class="home-container">
+        <!-- 头部区域 -->
+      <el-header>
+          <div>
+              <img src="../assets/logo.png" alt="">
+              <span>电商后台管理系统</span>
+          </div>
+          <el-button type="info" @click="logout">退出</el-button>
+      </el-header>
+      <!-- 页面主题区域 -->
+      <el-container>
+          <!-- 侧边栏 -->
+        <el-aside :width="iscollapse ? '64px': '200px'">
+          <div class="toggle-button" @click="toggleCollapse">|||</div>
+    <el-menu
+      background-color="#333744"
+      text-color="#fff"
+      active-text-color="#409eff" unique-opened :collapse="iscollapse"
+      :collapse-transition="false" router 
+      :default-active= "activePath" >
+      <!-- 一级菜单 -->
+      <el-submenu :index="item.id+''" v-for="item in menulist" 
+      :key="item.id" >
+          <!-- 一级菜单模板区 -->
+        <template slot="title">
+            <!-- 图标 -->
+          <i :class="iconsObj[item.id]"></i>
+          <!-- 文本 -->
+          <span>{{item.authName}}</span>
+        </template>
+       <!-- 二级菜单 -->
+         <el-menu-item :index="'/'+subItem.path" v-for="subItem in item.children" 
+         :key="subItem.id"  @click="saveNavState('/'+subItem.path)">
+             <template slot="title">
+            <!-- 图标 -->
+          <i class="el-icon-menu"></i>
+          <!-- 文本 -->
+          <span>{{subItem.authName}}</span>
+        </template>
+         </el-menu-item>
+      </el-submenu>
+     
+    </el-menu>
+        </el-aside>
+        <!-- 内容主体 -->
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  
 </template>
 <script>
 export default {
-    methods:{
-        logout(){
-            window.sessionStorage.clear()
-            this.$router.push('/login');
-        }
+    data(){
+        return{
+        //左侧菜单数据
+        menulist:[],
+        iconsObj:{
+            '125':'iconfont icon-user',
+            '103':'iconfont icon-tijikongjian',
+            '101':'iconfont icon-shangpin',
+            '102':'iconfont icon-danju',
+            '145':'iconfont icon-baobiao'
+        },
+        iscollapse:false,
+        //被激活的连接地址
+        activePath:''
+    }},
+    created(){
+        this.getMenuList()
+        this.activePath=window.sessionStorage.getItem('activePath')
+    },
+  methods: {
+    logout() {
+      window.sessionStorage.clear();
+      this.$router.push("/login");
+    },
+    //获取所有的菜单
+    async getMenuList(){
+        const {data:res}=await this.$http.get('menus')
+        if(res.meta.status!==200)return this.$message.error()
+        this.menulist=res.data
+        console.log(res)
+    },
+    //点击按钮，折叠展开
+    toggleCollapse(){
+      this.iscollapse=!this.iscollapse;
+    },
+    //保存连接激活状态
+    saveNavState(activePath){
+      window.sessionStorage.setItem('activePath',activePath)
+      this.activePath=activePath
     }
+  }
 }
 </script>
-<style lang="lang="less" scoped">
+<style lang="less" scoped>
+.home-container{
+    height: 100%;
 
+}
+.el-header {
+    background-color: #373d41;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
+    font-size: 20px;
+    color: #fff;
+        >div{
+            display: flex;
+            align-items: center;
+        }
+        img{
+        height: 40px;
+        width: 40px;
+        opacity:0.8; 
+        }
+        span{
+            margin-left: 15px;
+        }
+
+    
+}
+
+.el-aside{
+    background-color: #333744;
+    .el-menu{
+        border-right: none;
+    }
+}
+.iconfont{
+    margin-right: 10px;
+}
+.toggle-button{
+  background-color:#4a5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+
 
 //导入全局样式表
 import './assets/css/global.css'
@@ -10,6 +13,12 @@ import './assets/css/global.css'
 import axios from 'axios'
 //配置请求的跟路径
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+//为请求头对象，添加token验证的Authorization字段
+axios.interceptors.request.use(config=>{
+  config.headers.Authorization=window.sessionStorage.getItem('token')
+  //必须return  config
+  return config;
+})
 // 挂在到Vue实例，后面可通过this调用
 Vue.prototype.$http = axios
 
